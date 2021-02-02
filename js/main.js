f22d12be0db67acef0fe7172fe9a665a4abeddce
Alex Korobov <devasko@ro.ru>
2021-02-02 16:18:02 +0300

    Новые цвета+шрифты

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
 $( document ).ready( function () {
-    $( window ).scroll(function () {
-       if ( $( this ).scrollTop() > 500 ) {
-           $( '.scrollTop' ).fadeIn();
-       } else {
-           $( '.scrollTop' ).fadeOut();
-       }
-    });
-
-    $( '.scrollTop' ).click(function () {
-        $( 'html, body' ).animate({scrollTop : 0}, 800);
-    })
+    
 });

9e697729ed9e43ba0dad8f13675990af97fbfc35
Alex Korobov <devasko@ro.ru>
2021-02-03 19:06:44 +0300

    Базово адаптированное меню

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 // Скрипты страниц
 
 $( document ).ready( function () {
+    // Кнопка прокруткт вверх
     $( window ).scroll(function () {
         if ( $( this ).scrollTop() > 500 ) {
             $( '.scrollTop' ).fadeIn();
+
+            $( '.top-nav__list.show' ).fadeOut();
+
         } else {
             $( '.scrollTop' ).fadeOut();
         }
@@ -13,12 +17,23 @@ $( document ).ready( function () {
         $( 'html, body' ).animate({scrollTop : 0}, 800);
     });
 
+    //  Бургер главного меню
+    const burger = document.getElementById('burger' );
+    const times = document.getElementById('times' );
+    const menu = document.getElementById('nav__list' );
+    burger.addEventListener('click', () => {
+        menu.classList.toggle('show');
+
+        if ( $( 'top-nav__list' ).hasClass( 'show' ) ) {
+            $( '.top-nav__list.show' ).fadeOut();
+        } else {
+            $( '.top-nav__list.show' ).fadeIn();
+        }
+    });
+
+    times.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
 
 });
 
-
-const burger = document.getElementById('burger');
-const menu = document.getElementById('nav__list');
-burger.addEventListener('click', () => {
-    menu.classList.toggle('show');
-});

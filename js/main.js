c42ac83b90cec2c58e8540e9ca3f456d46cde3ba
Alex Korobov <devasko@ro.ru>
2021-02-03 15:42:51 +0300

    Добавлена секция About

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+// Скрипты страниц
+
 $( document ).ready( function () {
-    
+    $( window ).scroll(function () {
+        if ( $( this ).scrollTop() > 500 ) {
+            $( '.scrollTop' ).fadeIn();
+        } else {
+            $( '.scrollTop' ).fadeOut();
+        }
+    });
+
+    $( '.scrollTop' ).click(function () {
+        $( 'html, body' ).animate({scrollTop : 0}, 800);
+    });
+
+
+});
+
+
+const burger = document.getElementById('burger');
+const menu = document.getElementById('nav__list');
+burger.addEventListener('click', () => {
+    menu.classList.toggle('show');
 });
